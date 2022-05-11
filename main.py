b407631c7cb7ce3921478bbdfde480f674f67382
LomasovEgor <egorka.lomasov@gmail.com>
2022-05-11 16:41:00 +0300

    graph drawing test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyvis.network import Network
+from pyvis.network import Network, Edge
 from network_data import NetworkData
 
 network = Network('600px', '600px', notebook=True, )
@@ -19,13 +19,13 @@ for values in data.test_data.values():
 
 network.add_nodes(nodes=nodes, label=labels, title=titles, color=colors)
 
-print(nodes)
-print(labels)
-print(titles)
-print(colors)
 
-#
-# network.add_edges([(1, 2, 10), (2, 3, 5), (3, 4, 5), (3, 5, 2), (2, 6, 2), (6, 7, 2),
-#                    (6, 8, 2), (6, 9, 2), (6, 10, 2)])
+edges = [(1, 2, 10), (2, 3, 5), (3, 4, 5), (3, 5, 2), (2, 6, 2), (6, 7, 2),
+                   (6, 8, 2), (6, 9, 2), (6, 10, 2)]
 
+# network.add_edge(1, 2, title='10')
+
+network.add_edges(edges)
+
+network.show_buttons(filter_=True)
 network.show('test_network.html')
