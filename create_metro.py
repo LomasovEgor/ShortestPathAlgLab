0e7576fc545be256e3403cb0af79b8c989427c39
LomasovEgor <egorka.lomasov@gmail.com>
2022-05-14 01:57:37 +0300

    create_metro.py creates a graph for the St. Petersburg metro and writes to Saint-Petersburg_Metro_graph.json

--- create_metro.py
+++ create_metro.py
@@ -1,7 +1,4 @@
 from metro_graph_builder import Metro
-from dijkstra_algorithm import dijkstra_algorithm
-from UI import print_result
-from UI import StationEnterWin
 
 SaintP_metro = Metro()
 SaintP_metro.create_line(['Парнас',
@@ -42,6 +39,53 @@ SaintP_metro.create_line(['Девяткино',
                           'Ленинский проспект',
                           'Проспект ветеранов'], 3)
 
-SaintP_metro.add_edge('Технологический институт 2', 'Технологический институт 1', 1)
+SaintP_metro.create_line(['Беговая',
+                          'Новокрестовская',
+                          'Приморская',
+                          'Василеостровская',
+                          'Гостиный двор',
+                          'Маяковская',
+                          'Площадь александра невского 1',
+                          'Елизаровская',
+                          'Ломоносовская',
+                          'Пролетарская',
+                          'Обухово',
+                          'Рыбацкое'], 3)
 
-SaintP_metro.save_metro('testMetro.Json')
+SaintP_metro.create_line(['Спасская',
+                          'Достоевская',
+                          'Лиговский проспект',
+                          'Площадь александра невского 2',
+                          'Новочеркасская',
+                          'Ладожская',
+                          'Проспект большевиков',
+                          'Улица дыбенко'], 3)
+
+SaintP_metro.create_line(['Комендантский проспект',
+                          'Старая деревня',
+                          'Крестовский остров',
+                          'Чкаловская',
+                          'Спортивная',
+                          'Адмиралтейская',
+                          'Садовая',
+                          'Звенигородская',
+                          'Обводный канал',
+                          'Волковская',
+                          'Бухарестская',
+                          'Международная',
+                          'Проспект славы',
+                          'Дунайская',
+                          'Шушары'], 3)
+
+SaintP_metro.add_edge('Технологический институт 2', 'Технологический институт 1', 1)
+SaintP_metro.add_edge('Невский проспект', 'Гостиный двор', 1)
+SaintP_metro.add_edge('Площадь восстания', 'Маяковская', 1)
+SaintP_metro.add_edge('Площадь александра невского 1', 'Площадь александра невского 2', 1)
+SaintP_metro.add_edge('Достоевская', 'Владимирская', 1)
+SaintP_metro.add_edge('Звенигородская', 'Пушкинская', 1)
+SaintP_metro.add_edge('Садовая', 'Спасская', 1)
+SaintP_metro.add_edge('Садовая', 'Сенная площадь', 1)
+SaintP_metro.add_edge('Сенная площадь', 'Спасская', 1)
+
+
+SaintP_metro.save_metro('Saint-Petersburg_Metro_graph.json')
